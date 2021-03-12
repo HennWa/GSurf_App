f42e9fa0e448098fe8418ad6f98361c72b4d77ed
HennWa <henning.wagner1@googlemail.com>
2021-03-12 12:28:42 +0100

    ButterworthFilter extended with new function for sequences

--- app/src/main/java/com/example/gsurfexample/utils/algorithms/DigitalButterworthFilter.java
+++ app/src/main/java/com/example/gsurfexample/utils/algorithms/DigitalButterworthFilter.java
@@ -1,19 +1,18 @@
 package com.example.gsurfexample.utils.algorithms;
 
-import android.util.Log;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Digital Butterworth Filter with fixed coefficients, which can be applied to float array.
  * (assumed parameters are determined for given equidistant sample rate of values)
  */
 public class DigitalButterworthFilter {
-    float samplePeriod;
     float[] nominator;
     float[] denominator;
+
+    float[] xMemo;  // Input memory
+    float[] yMemo;  // Output memory
 
     /**
      * Creates Digital Butterworth Filter with given coefficients.
@@ -24,6 +23,8 @@ public class DigitalButterworthFilter {
     public DigitalButterworthFilter(float[] nominator, float[] denominator) {
         this.nominator = nominator;
         this.denominator = denominator;
+        this.xMemo = new float[nominator.length];
+        this.yMemo = new float[nominator.length];
     }
 
     /**
@@ -54,4 +55,40 @@ public class DigitalButterworthFilter {
         return y;
     }
 
+    /**
+     * Apply Digital Butterworth Filter to a sequence of arrays, whereas no time vector is used.
+     * Instead samples are assumed to be equidistant and comply with sample rate for which the
+     * filter is designed.
+     * Formel: y[n]=−a1y[n−1]−a2y[n−2]−…−aNy[n−N]+
+     *              +b0x[n]+b1x[n−1]+…+bNx[n−N]
+     *
+     * @param x signal to be filtered (one array of sequence).
+     */
+    public float[] filterSequence(float[] x){
+
+        float[] y = new float[x.length];
+
+        // Append entries of memory
+        float[] xMemoX = Arrays.copyOf(xMemo, xMemo.length + x.length);
+        System.arraycopy(x, 0, xMemoX, xMemo.length, x.length);
+        float[] yMemoY = Arrays.copyOf(yMemo, yMemo.length + y.length);
+        System.arraycopy(y, 0, yMemoY, yMemo.length, y.length);
+
+        for(int i=xMemo.length; i<xMemoX.length; i++){
+            for(int j=0; j<nominator.length; j++){
+                yMemoY[i] += nominator[j] * xMemoX[i-j];
+            }
+            for(int j=0; j<denominator.length-1; j++){     // first denominator always 1 -> skipped
+                yMemoY[i] -= denominator[j+1] * yMemoY[i-(j+1)]; // first denominator always 1 -> skipped
+            }
+        }
+
+        // Store last N entries in memory
+        System.arraycopy(xMemoX, xMemoX.length-xMemo.length, xMemo, 0, xMemo.length);
+        System.arraycopy(yMemoY, yMemoY.length-yMemo.length, yMemo, 0, yMemo.length);
+
+        // Cut out result
+        System.arraycopy(yMemoY, yMemoY.length-y.length, y, 0, y.length);
+        return y;
+    }
 }
--- app/src/test/java/com/example/gsurfexample/DigitalButterworthFilterTest.java
+++ app/src/test/java/com/example/gsurfexample/DigitalButterworthFilterTest.java
@@ -1,8 +1,12 @@
 package com.example.gsurfexample;
 
+import android.util.Log;
+
 import com.example.gsurfexample.utils.algorithms.DigitalButterworthFilter;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,9 +19,10 @@ public class DigitalButterworthFilterTest {
     @Test
     public void testFilter() {
 
+        // Test 1
         float testNominator[] = {0.00782021f, 0.01564042f, 0.00782021f};
         float testDenominator[] = {1.f, -1.73472577f,  0.7660066f};
-        float x[] = {1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f, 9f, 10f, 10f, 10f, 11f, 12f, 13f, 14f, 15};
+        float x[] = {1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f, 9f, 10f, 10f, 10f, 11f, 12f, 13f, 14f, 15f};
         float y[] ={7.82020803e-03f, 4.48467485e-02f, 1.34368144e-01f, 2.92581472e-01f,
                     5.29745063e-01f, 8.51247233e-01f, 1.25857729e+00f, 1.75019128e+00f,
                     2.32227006e+00f, 2.96937113e+00f, 3.67715854e+00f, 4.41711210e+00f,
@@ -28,6 +33,25 @@ public class DigitalButterworthFilterTest {
 
         for(int i=0; i<y.length; i++){
             assertEquals("Digital ButterWorthFilter error",y[i], yRes[i], 1e-5f);
+        }
+
+        // Test 2
+        DigitalButterworthFilter testFilter2 = new DigitalButterworthFilter(testNominator,testDenominator);
+        float xSeq1[] = {1f, 2f};
+        float xSeq2[] = {3f, 4f, 5f, 6f, 7f, 8f};
+        float xSeq3[] = {9f, 10f, 10f, 10f, 11f, 12f, 13f, 14f, 15f};
+
+        float[] yResSeq1 = testFilter2.filterSequence(xSeq1);
+        float[] yResSeq2 = testFilter2.filterSequence(xSeq2);
+        float[] yResSeq3 = testFilter2.filterSequence(xSeq3);
+
+        float[] yResSeq12 = Arrays.copyOf(yResSeq1, yResSeq1.length + yResSeq2.length);
+        System.arraycopy(yResSeq2, 0, yResSeq12, yResSeq1.length, yResSeq2.length);
+        float[] yRes2 = Arrays.copyOf(yResSeq12, yResSeq12.length + yResSeq3.length);
+        System.arraycopy(yResSeq3, 0, yRes2, yResSeq12.length, yResSeq3.length);
+
+        for(int i=0; i<y.length; i++){
+            assertEquals("Digital ButterWorthFilter error",y[i], yRes2[i], 1e-5f);
         }
     }
 }
